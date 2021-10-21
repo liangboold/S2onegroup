d0725f7a3d321202084e58e9ba7383771716c500
oldli <2320756871@qq.com>
2021-10-21 19:44:16 +0800

    网络框架

---
+++ Netlibrary/build/generated/source/buildConfig/debug/com/example/netlibrary/BuildConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.netlibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.netlibrary";
+  public static final String BUILD_TYPE = "debug";
+  public  static  final String BASEURL ="http://39.98.153.96:8080/";
+}
